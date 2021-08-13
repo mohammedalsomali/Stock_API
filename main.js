c3d73db1d8f7d8ddbe45e391ac87dda7007d2b59
mohammedalsomali <d.j.hamoody@gmail.com>
2021-08-13 02:24:02 -0500

    new

--- main.js
+++ main.js
@@ -1,72 +1,103 @@
+let xval = [];
+let yval = [];
+const stock = document.querySelector('#stockcode');
+const btn = document.querySelector('#lookup');
+var stockcod = 0;
 
 
-class getdata {
+btn.addEventListener('click', function() {
+	
+	
 
-	constructor() {
-		this.xvalues = [];
-		this.yvalues = [];
+	if(stock.value == 0){
+		alert('inter stock code');
 
 	}
-
-
-	fetchAPI() {
-		fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&outputsize=compact&apikey=MOR0CI3PU3IZ43EN",
-		)
-			.then(response => {
-				let x = response.json()
-				let data = JSON.stringify(x);
-
-
-				console.log(x);
-				return x
-			})
-			.then(data => {
-				console.log(data);
-				let apix = [];
-				let apiy = [];
-				for (var key in data['Time Series (Daily)']) {
-					apix.push(key);
-					apiy.push(data['Time Series (Daily)'][key]['1. open']);
-
-				}
-				this.xvalues = apix;
-				this.yvalues = apiy;
-
-			});
-		
-			
-
+	else {
+		stockcod = stock.value;
 		sketch();
 	}
+	
+
+} )
+
+
+
+async function fetchAPI() {
+
+
+
+	await fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="+ stockcod + "&outputsize=compact&apikey=MOR0CI3PU3IZ43EN",
+	)
+		.then(response => {
+			let x = response.json()
+			let data = JSON.stringify(x);
+
+
+			console.log(x);
+			return x
+		})
+		.then(data => {
+			console.log(data);
+			// let apix = [];
+			// let apiy = [];
+			for (var key in data['Time Series (Daily)']) {
+				xval.push(key);
+				yval.push(data['Time Series (Daily)'][key]['1. open']);
+
+			}
+			// xval = apix;
+			// yval = apiy;
+			console.log(xval, yval);
+		});
+
+	
 
 
 }
 
 
-var stock = new getdata();
-stock.fetchAPI();
 
 
-function sketch() { 
 
+
+
+async function sketch() { 
+	
+	
+
+
+
+	await fetchAPI();
+	console.log(xval, yval);
 	const ctx = document.getElementById('chart').getContext('2d');
 	const myChart = new Chart(ctx, {
-
-		type: 'bar',
+		
+		
+	
+		
+		type: 'line',
 		data: {
-			labels: stock.xvalues,
+			labels: xval,
 			datasets: [
 			{
 				label: 'stock data',
-				data: stock.yvalues,
-				backgroundColor: ['rgba(255, 99, 132, 0.2)',],
+				data: yval,
+				backgroundColor: 'yellow',
 				borderWidth: 1
 			}]
 		}
 
 	});
+	Chart.destroy();
+	
+	
+
 }
 
-sketch();
+
+
+
+
 
 
