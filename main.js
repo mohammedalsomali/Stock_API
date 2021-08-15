ccee29048d5a223290438fb31d6e9d2c2cdd4c9d
mohammedalsomali <d.j.hamoody@gmail.com>
2021-08-15 17:35:43 -0500

    graphing complete

--- main.js
+++ main.js
@@ -4,12 +4,81 @@ const stock = document.querySelector('#stockcode');
 const btn = document.querySelector('#lookup');
 var stockcod = 0;
 var myChart;
-const datatype = document.querySelector('#subject');
+const stocktype = document.querySelector('#subject');
+const timeintervalch = document.querySelector('#timeint');
+const historych = document.querySelector('#history');
+const personalAPIch = document.querySelector('#API');
+
+var sear = '';
+var interval = '';
+var openor;
 var chioce = 0;
+var timeInt = 0;
+var history = 0;
+var Api_key = 'MOR0CI3PU3IZ43EN';
 
 
-datatype.addEventListener('change', function(){
-	chioce = datatype.value
+
+stocktype.addEventListener('change', function(){
+	chioce = stocktype.value
+	// console.log(chioce);
+
+})
+
+
+
+timeintervalch.addEventListener('change', function(){
+	if (timeintervalch.value == "daily"){ 
+		if (chioce == 'stock'){
+			timeInt = 'TIME_SERIES_DAILY';
+			sear = 'Time Series (Daily)';
+		}
+		else if (chioce == 'crypto'){
+			timeInt = 'DIGITAL_CURRENCY_DAILY';
+			sear = 'Time Series (Digital Currency Daily)';
+			openpr = '1b. open (USD)';
+			
+
+		}
+		
+
+	}
+	else if (timeintervalch.value == "hourly"){
+		interval = '&interval=60min';
+		if (chioce == 'stock'){
+			timeInt = 'TIME_SERIES_INTRADAY';
+			sear = 'Time Series (60min)';
+		}
+		else if (chioce == 'crypto'){
+			timeInt = 'CRYPTO_INTRADAY';
+			sear = 'Time Series Crypto (60min)';
+			openpr = '1. open';
+
+		}
+
+		
+	}
+	// console.log(chioce);
+
+})
+
+
+historych.addEventListener('change', function(){
+	if (historych.value == 'full'){
+		history = 'full';
+	}
+	else {
+		history = 'compact';
+
+	}
+	// console.log(chioce);
+
+})
+
+
+
+personalAPIch.addEventListener('change', function(){
+	// Api_key = personalAPIch.value
 	// console.log(chioce);
 
 })
@@ -21,8 +90,7 @@ btn.addEventListener('click', function() {
 	
 	if( typeof myChart != 'undefined'){
 		myChart.destroy();
-		xval.length = 0;
-		yval.length = 0;
+		
 
 
 	}
@@ -30,8 +98,8 @@ btn.addEventListener('click', function() {
 
 
 
-	if(stock.value == 0 || chioce == 0){
-		alert('inter stock code or pick a stock market first');
+	if(stock.value == 0 || chioce == 0 || timeInt == 0 || history == 0){
+		alert('inter stock code/ pick a stock market first/ a time interval/  history');
 
 	}
 	else {
@@ -47,26 +115,31 @@ btn.addEventListener('click', function() {
 async function fetchstockAPI() {
 
 
-	await fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ stockcod + '&outputsize=compact&apikey=MOR0CI3PU3IZ43EN',
+	await fetch('https://www.alphavantage.co/query?function=' + timeInt + '&symbol=' + stockcod + interval + '&outputsize= ' + history + '&apikey=' + Api_key ,
 	)
 		.then(response => {
 			let x = response.json()
 			let data = JSON.stringify(x);
+			xval.length = 0;
+			yval.length = 0;
 
 
-			// console.log(x);
+			console.log(x);
 			return x
 		})
 		.then(data => {
-			// console.log(data);
+			console.log(data);
 		
-			for (var key in data['Time Series (Daily)']) {
+			for (var key in data[sear]) {
+				console.log(0);
 				xval.unshift(key);
-				yval.unshift(data['Time Series (Daily)'][key]['1. open']);
+				yval.unshift(data[sear][key]['1. open']);
 
 			}
-			// console.log(xval, yval);
+			console.log(xval, yval);
 		});
+
+		interval = '';
 	
 
 	
@@ -77,27 +150,29 @@ async function fetchstockAPI() {
 async function fetchcryptoAPI() {
 
 
-	await fetch('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=' + stockcod + '&market=USD&apikey=MOR0CI3PU3IZ43EN',
+	await fetch('https://www.alphavantage.co/query?function=' + timeInt + '&symbol=' + stockcod + '&market=USD' + interval + '&apikey=' + Api_key,
 	)
 		.then(response => {
 			let x = response.json()
 			let data = JSON.stringify(x);
+			xval.length = 0;
+			yval.length = 0;
 
-
-			// console.log(x);
+			console.log(x);
+			console.log(data);
 			return x
 		})
 		.then(data => {
-			// console.log(data);
 		
-			for (var key in data['Time Series (Digital Currency Daily)']) {
+			for (var key in data[sear]) {
+				// console.log(0);
 				xval.unshift(key);
-				yval.unshift(data['Time Series (Digital Currency Daily)'][key]['1b. open (USD)']);
+				yval.unshift(data[sear][key][openpr]);
 
 			}
 			
 		});
-		// console.log(xval, yval);
+		console.log(xval, yval);
 
 	
 
