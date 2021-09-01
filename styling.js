0a6f775015f94dfb8b7c2ae3565b6c2146a0d155
mohammedalsomali <d.j.hamoody@gmail.com>
2021-09-01 14:57:59 -0500

    new css

--- styling.js
+++ styling.js
@@ -1,4 +1,5 @@
 //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
@@ -13,3 +14,28 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+
+//   interact('.newsdiv')
+//     .resizable({
+//       edges: { top: true, left: true, bottom: true, right: true },
+//       listeners: {
+//         move: function (event) {
+//           let { x, y } = event.target.dataset
+
+//           x = (parseFloat(x) || 0) + event.deltaRect.left
+//           y = (parseFloat(y) || 0) + event.deltaRect.top
+
+//           Object.assign(event.target.style, {
+//             width: `${event.rect.width}px`,
+//             height: `${event.rect.height}px`,
+//             transform: `translate(${x}px, ${y}px)`
+//           })
+
+//           Object.assign(event.target.dataset, { x, y })
+//         }
+//       }
+//     })
+
+// });
